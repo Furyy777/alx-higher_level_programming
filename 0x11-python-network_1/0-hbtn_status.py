4faf57c2b7f24a0556634aca985190fc2f54b629
Anassselasri <anaselasri37@gmail.com>
2024-03-28 23:50:45 +0000

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,8 @@
 """Fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
-def main():
+
+if __name__ == "__main__":
     request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         body = response.read()
@@ -10,6 +11,3 @@ def main():
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
-
-if __name__ == "__main__":
-    main()
